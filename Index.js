5c97c1d5d12149a7f59a267a1fb140e00cdd75f8
louiswand <louiswand@hotmail.com>
2023-08-14 22:59:18 +0100

    first commit

---
+++ Index.js
@@ -0,0 +1,15 @@
+// this is a test doc 
+console.log('hello world');
+
+// playing with variables 
+
+let firstName = 'louis'
+console.log(firstName);
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate)
+
+
+
+
